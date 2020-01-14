2824a5d19f04ab894cbfcf454434d3b61ea19458
mihalichpalich <pavlovtfspb@gmail.com>
2020-01-15 01:52:57 +0300

    hhfhfhfhf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
-  return <div></div>
+  const [todos, setTodos] = React.useState([ //todos - state, setTodos - ф-я, изменяющая state
+    {
+      id: 1,
+      completed: false,
+      title: 'Купить хлеб'
+    },
+    {
+      id: 2,
+      completed: false,
+      title: 'Купить масло'
+    },
+    {
+      id: 3,
+      completed: false,
+      title: 'Купить молоко'
+    },
+  ])
+
+  function toggleTodo(id) {
+    setTodos(
+        todos.map(todo => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed
+          }
+          return todo
+    }))
+  }
+  
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+  
+  function addTodo(title) {
+    setTodos(todos.concat([
+      {
+        title,
+        id: Date.now(),
+        completed: false
+      }
+    ]))
+  }
+  
+  return (
+      <Context.Provider value={{removeTodo}}>
+          <div className="wrapper">
+            <h1>React tutorial</h1>
+            <AddTodo onCreate={addTodo}/>
+            {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p>No todos!</p>}
+          </div>
+      </Context.Provider>
+  )
 }
 
 export default App;
